f689bab9c8468bb90a7251aa3f26c812f4ebd360
un <un@DESKTOP-JAU235M>
2025-08-20 21:33:41 +0300

    Academy MissionComplete

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -365,14 +365,14 @@ Human** Load(const std::string& filename, int& n)
 		cout << "Position " << fin.tellg() << endl;
 
 		//4) Загружаем объекты из файла:
-		for(int i =0; !fin.eof();)
+		for(int i =0; i<n;i++)
 		{
 			std::string buffer;
 			std::getline(fin, buffer, ':');
 			if (buffer.size() < 5)continue;
 			group[i] = HumanFactory(buffer);
 			fin >> *group[i];
-			i++;
+			/*i++;*/
 		}
 	}
 	else
@@ -510,11 +510,9 @@ void main()
 		cout << "Deleted object #" << i << endl;
 	}
 #endif // DEBUG
-	int n;
+	int n = 0;
 	Human** group = Load("group.txt", n);
 	Print(group, n);
-
-
+	Save(group, n, "group2.txt");
 	Clear(group, n);
 }
-
